190f2145a768c54560421377bed9f1aa0bf8a61a
Casey Singleton <csinglet@e1z2r7p2.42.us.org>
2020-01-31 16:14:08 -0800

    Added documentation

--- sources/map.c
+++ sources/map.c
@@ -13,6 +13,14 @@
 #include "Wolf3D.h"
 #include "map.h"
 
+/*
+** Function: map_init
+** Allocates memory for a t_map structure and initializes all memory to NULL.
+**
+** path: The file path to a map file
+**
+** Return: A t_map structure to be initialized with map information.
+*/
 t_map			*map_init(char *path)
 {
 	t_map		*map;
@@ -29,6 +37,15 @@ t_map			*map_init(char *path)
 	return (map);
 }
 
+/*
+** Function: open_file
+** Trys to open the maps file path given by the user. On error the program
+** exits.
+**
+** file_name: The path to the map file
+**
+** Return: Integer that is the file descriptor of the opened map file
+*/
 static int		open_file(const char *file_name)
 {
 	int			fd;
@@ -41,6 +58,15 @@ static int		open_file(const char *file_name)
 	return (fd);
 }
 
+/*
+** Function: map_info_init
+** Allocates memory for the double integer array used to represent the 2D map.
+**
+** m: A t_map structure containing information about the current map. Used here
+**    for its height and width information.
+**
+** Return: A double integer array used to represent a 2D map.
+*/
 static int		**map_info_init(t_map *m)
 {
 	int			**info;
@@ -55,6 +81,17 @@ static int		**map_info_init(t_map *m)
 	return (info);
 }
 
+/*
+** Function: atoi_line
+** Converts a line of numbers used to represent on line of the 2D map from
+** character values into integer values.
+**
+** m: A t_map structure containing information about the current map
+** line: The line of characters to be converted to integers
+** position: The Y position of the current line
+**
+** Returns: None
+*/
 static void		atoi_line(t_map *m, char *line, int position)
 {
 	char		**numbers;
@@ -75,6 +112,18 @@ static void		atoi_line(t_map *m, char *line, int position)
 	free(numbers);
 }
 
+/*
+** Function: get_map
+** Opens the given map, initializes the t_map structure, then convers the ascii
+** values from the open file into integer values and stores those values into
+** a 2D map represented as an int[][]. If an error occures, the memory is freed
+** and NULL is returned.
+**
+** path: The path to the map file
+**
+** Return: On success, a fully initialized t_map structure. On failure, NULL is
+** returned.
+*/
 t_map			*get_map(char *path)
 {
 	t_map		*map;
